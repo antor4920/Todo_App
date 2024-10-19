8a2a30bae76c0478a43502bae9c6f638bfe9a5eb
antor4920 <antor4920@gmail.com>
2024-10-19 10:52:21 +0600

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Mapping from "./Mapping";
+import NewTodos from "./NewTodos";
+
+const Home = () => {
+  return (
+    <div>
+      <NewTodos />
+      <Mapping />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/NewTodos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewTodos = () => {
+  return <div>NewTodos</div>;
+};
+
+export default NewTodos;
